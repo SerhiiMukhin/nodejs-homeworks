6a260ccb650cf0b1281c0b46d7e1ad3d971cf95a
SerhiiMukhin <110244388+SerhiiMukhin@users.noreply.github.com>
2023-06-15 22:00:51 +0300

    Added the ability to upload a user's avatar

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,14 +1,15 @@
 const express = require('express');
-const { validateBody, authenticate } = require('../../middlewares');
+const { validateBody, authenticate, userSchemas, upload } = require('../../middlewares');
 const router = express.Router();
-const { userSchemas } = require('../../middlewares');
 
 const userController = require('../../controllers/auth-controller');
 
 router.post('/register', validateBody(userSchemas.registerSchema), userController.register);
 router.post('/login', validateBody(userSchemas.loginSchema), userController.login);
 
-router.get("/current", authenticate, userController.getCurrent)
-router.post("/logout", authenticate, userController.logout)
+router.get('/current', authenticate, userController.getCurrent);
+router.post('/logout', authenticate, userController.logout);
+
+router.patch('/avatars', authenticate, upload.single('avatar'), userController.changeAvatar);
 
 module.exports = router;
